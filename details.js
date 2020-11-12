fc6ef41bae9f8d20cfb6a82b2ad30050310e8d39
krista-t <krista.tomicicrgmail.com>
2020-11-12 18:50:24 +0100

    cards working

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 const detailPage =
-   "https://javasquipt.com/wp-json/wp/v2/detail_page/141";
+   "https://javasquipt.com/wp-json/wp/v2/detail_page/117";
  
 
 window.addEventListener("DOMContentLoaded", showDetail);
@@ -16,31 +16,53 @@ function showDetail() {
 }
 
 
+
 function showSingleDetail(detail) {
+
+  //LOOP THROUGH
+
+  detail.content_card.forEach(card => {
+    console.log(card)
+    const template = document.querySelector("#cardTemplate").content;
+    const clone = template.cloneNode(true); 
+     
+    console.log(clone)
+     clone.querySelector(".card-one h3").textContent = card.post_title;
+
+     clone.querySelector(".card-one p").innerHTML = card.post_content;
+
+     if(card.button_label == ""){
+      clone.querySelector(".btn").style.display = "none";
+     }else {
+      clone.querySelector(".btn button").innerHTML = card.button_label;
+     }
+     
+     document.querySelector("#container").appendChild(clone);
+
+  });
 
   //SHOW TEXT FROM DATA
 
-  //card-one
-  const title = document.querySelector("h1");
-  title.innerHTML = detail.title.rendered;
+//   //card-one
+//   const title = document.querySelector("h1");
+//   title.innerHTML = detail.title.rendered;
 
-  const description = document.querySelector("h2");
-  description.innerHTML = detail.content.rendered;
- 
+//   const description = document.querySelector("#paragraph");
+//   description.innerHTML = detail.content.rendered;
+//  console.log(detail)
 
-  const subtitle = document.querySelector("h3");
-  subtitle.innerHTML = detail.content_card[0].post_title;
+//   const subtitle = document.querySelector(".card-one h3");
+//   subtitle.innerHTML = detail.content_card[0].post_title;
 
-  const cardText = document.querySelector("p");
-  cardText.innerHTML =  detail.content_card[0].post_content["wp:paragraph"]; //not displaying
+//   const cardText = document.querySelector(".card-one p");
+//   cardText.innerHTML =  detail.content_card[0].post_content; //not displaying
 
-  const button = document.querySelector("button");
-  button.textContent = detail.content_card[0].button_label;
+//   const button = document.querySelector("button");
+//   button.textContent = detail.content_card[0].button_label;
 
-  //card-two
-  subtitle.innerHTML = detail.content_card[1].post_title;
-  cardText.innerHTML =  detail.content_card[1].post_content["wp:paragraph"];
-
+//   //card-two
+//    document.querySelector(".card-two h3").innerHTML = detail.content_card[1].post_title;
+//    document.querySelector(".card-two p").innerHTML =  detail.content_card[1].post_content;
 
 }
    
